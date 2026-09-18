b13d8d34a4f452bb421e01510712157bc4297ad8
rahmat342 <rahmatrahmat342@gmail.com>
2026-09-18 10:38:28 +0700

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Portofolio_rmdh/",
+
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
 });
